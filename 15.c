39a7081c02490c0a95ace3714a9cf5a07e91816e
Akshada Ranalkar <akshadaranalkar27@gmail.com>
2025-06-05 00:45:36 -0500

    Added While loop program

---
+++ 15.c
@@ -0,0 +1,18 @@
+/*
+While Loop:
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    while (iCnt < 4)
+    {
+        printf("This is a while loop program\n");
+        iCnt++;
+    }
+
+    return 0;
+}
